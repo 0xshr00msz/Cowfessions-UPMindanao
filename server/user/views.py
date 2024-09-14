cf90c20a7908f70f617407acbdd315ab9539d22f
0xshr00msz <yldeviermagpusao@gmail.com>
2024-09-14 14:35:09 +0800

    feat: add user

---
+++ server/user/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+# Models
+from .models import User
+
+# Create your views here.
+
+def index(request):
+    pass
