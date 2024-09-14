9b1fb9a5628bd1a59314d86445b1a562edc5b387
0xshr00msz <yldeviermagpusao@gmail.com>
2024-09-14 14:38:44 +0800

    feat: initialize views and models; add urls and util (helper)

--- server/cowfessions/models.py
+++ server/cowfessions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
+
+class Cowfession(models.Model):
+
+    cowfession_text = models.CharField(max_length=256)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.cowfession_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
